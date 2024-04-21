847d792f9c0461672d42675a85fdf68cefc920fa
Pavan <129081053+Pavanpraja@users.noreply.github.com>
2024-04-21 15:57:42 +0530

    some updation

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,9 +4,9 @@ import '../../App.css'
 
 const Home = () => {
   return (
-    <div className="p-[1rem] transp-appbac [box-shadow:0rem_0rem_1rem_0rem_gray] w-[60%] max-[600px]:w-[70vw] h-[95%]">
+    <div className="p-[1rem] transp-appbac [box-shadow:0rem_0rem_1rem_0rem_gray] w-[60%] max-[600px]:w-[70vw]">
     <h1 className="mb-[0.2rem] font-bold text-[1.5rem] [text-shadow:0_0_0.2rem_black] text-white ml-6 border-b border-black flex space-x-2 items-center pb-[0.2rem]"><img src="/bacicon.png" alt="icon" width={40} /><p>ChatterSparkle</p></h1>
-      <div className="flex rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 h-[50rem]">
+      <div className="flex rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
         <Sidebar />
         <MessageContainer />
       </div>
